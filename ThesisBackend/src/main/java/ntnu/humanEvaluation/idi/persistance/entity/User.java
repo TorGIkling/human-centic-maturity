e75aa13c89931b1f5be60324e0554866a43a1e36
Tor Gikling <torog@stud.ntnu.no>
2024-12-29 18:00:08 +0100

    Updated entity class

--- ThesisBackend/src/main/java/ntnu/humanEvaluation/idi/persistance/entity/User.java
+++ ThesisBackend/src/main/java/ntnu/humanEvaluation/idi/persistance/entity/User.java
@@ -1,8 +1,6 @@
 package ntnu.humanEvaluation.idi.persistance.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -12,7 +10,6 @@ import lombok.*;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
     private String username;
     private String email;
